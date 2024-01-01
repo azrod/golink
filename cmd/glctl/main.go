f3593f6e471115aec9ca7ab5ea5c3cd90bd00912
Mickael Stanislas <contact@mickael-stanislas.com>
2024-01-01 18:28:58 +0100

    feat: add install script

---
+++ cmd/glctl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/azrod/golink/cmd/glctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/azrod/golink/cmd/cmd"
-
-func main() {
-	cmd.Execute()
-}
